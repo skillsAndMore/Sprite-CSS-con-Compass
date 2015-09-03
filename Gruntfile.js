50564e4300757f28e58bd52c3d2869e8cd981e96
Andrea Barghigiani <a.barghigiani@gmail.com>
2015-09-03 22:52:42 +0200

    Progetto Iniziale
    Questo è lo stato del progetto prima ancora di fare qualsiasi
    esperimento.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt){
+
+    //Configuro Grunt
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass',
+                    cssDir: '.',
+                    imagesDir: 'img/',
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*scss',
+                tasks: ['compass']
+            }
+        }
+    });
+
+    //Creo i Task da Eseguire
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+}
